e465b9a9ce37ba16688597ea91f0e8a96f6502a2
Lucas Alves Racoci <racoci.0@gmail.com>
2015-03-05 13:46:40 -0300

    <Insira uma boa frase de comit aqui>

--- src/pt/c01interfaces/s01knowledge/s02app/actors/Enquirer.java
+++ src/pt/c01interfaces/s01knowledge/s02app/actors/Enquirer.java
@@ -6,6 +6,8 @@ import pt.c01interfaces.s01knowledge.s01base.inter.IDeclaracao;
 import pt.c01interfaces.s01knowledge.s01base.inter.IEnquirer;
 import pt.c01interfaces.s01knowledge.s01base.inter.IObjetoConhecimento;
 import pt.c01interfaces.s01knowledge.s01base.inter.IResponder;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Enquirer implements IEnquirer
 {
@@ -20,24 +22,41 @@ public class Enquirer implements IEnquirer
 	public void connect(IResponder responder)
 	{
         IBaseConhecimento bc = new BaseConhecimento();
-        /* Recupera o arquivo txt do objeto */
-		obj = bc.recuperaObjeto("tiranossauro");
-		/* Pega a pergunta */
-		IDeclaracao decl = obj.primeira();
-		
-        boolean animalEsperado = true;
-		while (decl != null && animalEsperado) {
-			String pergunta = decl.getPropriedade();
-			String respostaEsperada = decl.getValor();
-			
-			String resposta = responder.ask(pergunta);
-			if (resposta.equalsIgnoreCase(respostaEsperada))
-				decl = obj.proxima();
-			else
-				animalEsperado = false;
-		}
-		
-		boolean acertei = responder.finalAnswer("tiranossauro");
+
+        String[] animais;
+        animais = bc.listaNomes();
+
+        HashMap<String, String> resps_adq;
+        resps_adq = new HashMap<String, String>();
+
+        boolean acertei = false;
+
+        /*for(int i = 0; i < animais.length; i++){
+            System.out.println(animais[i]);
+        }*/
+       /* Testar todos os animais */
+		for(int i = 0; i < animais.length && !acertei; i++) {
+            /* Recupera o arquivo txt do objeto */
+            obj = bc.recuperaObjeto(animais[i]);
+
+            /* Pega a pergunta */
+            IDeclaracao decl = obj.primeira();
+
+            boolean animalEsperado = true;
+            while (decl != null && animalEsperado) {
+                String pergunta = decl.getPropriedade();
+                String respostaEsperada = decl.getValor();
+
+                String resposta = responder.ask(pergunta);
+                if (resposta.equalsIgnoreCase(respostaEsperada))
+                    decl = obj.proxima();
+                else
+                    animalEsperado = false;
+            }
+           if(animalEsperado) {
+               acertei = responder.finalAnswer(animais[i]);
+           }
+        }
 		
 		if (acertei)
 			System.out.println("Oba! Acertei!");
