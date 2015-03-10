f7e45db480b32525a4b21f23eb03f146c1d29e34
Luizfrf3 <lf.fonseca@hotmail.com>
2015-03-10 16:40:42 -0300

    Últimos comentários, RACOCI, ver EnquirerMaze e OrchestratorGeral

---
+++ src/pt/c02classes/s01knowledge/s02app/app/OrchestratorGeral.java
@@ -0,0 +1,8 @@
+package pt.c02classes.s01knowledge.s02app.app;
+
+public class OrchestratorGeral {
+	/* Implementar um programa para funcionar com os dois jogos;
+	 * ele pede por terminal o nome do jogo e o nome do animal
+	 * ou do labirinto para realizar o jogo
+	 */
+}
